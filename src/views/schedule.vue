b765ef63e3391a714d4e627f2f49d1f0937ee1a9
yamaguchi.takuya <yamaguchi.takuya.lr@gmail.com>
2019-08-15 21:15:07 +0900

    Create schedule page. This page will be under construction at first deployment.

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>This is a schedule page</h1>
+  <div>
+    <mainHeader :text="'スケジュール'" />
+    <p>準備中です... 練習見学などご希望の方はお気軽に<router-link to="contact" class="nav-link">こちら</router-link>からお問い合わせください！</p>
+  </div>
 </template>
+
+<style lang="sass" scoped>
+.nav-link
+  margin: 0 0.5rem
+  font-weight: bold
+</style>
+
+<script>
+import mainHeader from "@/components/main_header";
+
+export default {
+  components: { mainHeader },
+}
+</script>
