5779a6e6dae066bff3c06804cea8f776c907409f
yamaguchi.takuya <yamaguchi.takuya.lr@gmail.com>
2019-08-16 11:42:06 +0900

    Update roster to 2019 vers.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="home">
-    <h1>This is home page.</h1>
+  <div>
+    <mainHeader text="BRANDINGSのホームページへようこそ！" />
+    <p>準備中です...</p>
+    <p>練習見学などご希望の方はお気軽に<navLink to="contact" label="こちら" />からお問い合わせください！</p>
   </div>
 </template>
 
+<style lang="sass" scoped>
+.nav-link
+  margin: 0 0.5rem
+  font-weight: bold
+</style>
+
 <script>
+import mainHeader from "@/components/main-header";
+import navLink from "@/components/nav-link";
+
+export default {
+  components: { mainHeader, navLink },
+}
 </script>
