5dcb76876f5975c2c4061a50fe4787c11d356e42
yamaguchi.takuya <yamaguchi.takuya.lr@gmail.com>
2019-08-16 10:15:01 +0900

    Not need two rows for wide display.

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <v-footer>
-      <a v-for="link in links.sns" :key="link.id" :href="link.url" target="_blank" class="snsLink">
-        <v-icon class="primary--text">{{ link.faIcon }}</v-icon>
-        <span class="d-none d-sm-inline font-weight-bold">{{ link.name }}</span>
-      </a>
-      <v-spacer></v-spacer>
-      <span class="primary--text font-weight-bold">&copy; {{ new Date().getFullYear() }} BRANDINGS</span>
-    </v-footer>
-    <v-footer>
-      <v-spacer></v-spacer>
-      <span class="primary--text">Powered by <a href="https://twitter.com/yamat47" target="_blank">@yamat47</a></span>
-      <v-spacer></v-spacer>
+    <v-footer flex row>
+        <div class="col-6 col-md-4">
+          <a v-for="link in links.sns" :key="link.id" :href="link.url" target="_blank" class="snsLink">
+            <v-icon class="primary--text">{{ link.faIcon }}</v-icon>
+            <span class="d-none d-sm-inline font-weight-bold">{{ link.name }}</span>
+          </a>
+        </div>
+        <span class="primary--text font-weight-bold col-6 col-md-4 text-right text-md-center">&copy; {{ new Date().getFullYear() }} BRANDINGS</span>
+        <span class="primary--text col-12 col-md-4 text-center text-md-right">Powered by <a href="https://twitter.com/yamat47" target="_blank">@yamat47</a></span>
     </v-footer>
   </div>
 </template>
