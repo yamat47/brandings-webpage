4a4cba53e17357308ef1775653f7f2666dad952f
yamaguchi.takuya <yamaguchi.takuya.lr@gmail.com>
2019-08-16 17:43:11 +0900

    Add blog content to Home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
     <p>日本プライベートフットボール協会（通称JPFF）に所属し、ハーバーボウルやオーシャンボウルといったボウルゲームでの勝利を目指して日々練習や試合に取り組んでいます。</p>
     <p>「アメフトはしたいけど週何日も練習に参加できない...。」「学生の頃ほどのきつい練習ではなく楽しくプレーしたい！」といった方にはぴったりのチームです。ぜひホームページでチームの雰囲気を感じていただき少しでも興味を持ってもらえると嬉しいです！</p>
     <navButton label="入部案内" to="join_us" />
+    <subHeader icon="fas fa-football-ball" text="チームブログの最新記事" />
+    <blogFeed :limit="3" />
+    <navButton label="BRANDINGSチームブログ" to="blog" />
   </div>
 </template>
 
@@ -20,8 +23,9 @@ import mainHeader from "@/components/main-header";
 import subHeader from "@/components/sub-header";
 import teamName from "@/components/team-name";
 import navButton from "@/components/nav-button";
+import blogFeed from "@/components/blog-feed";
 
 export default {
-  components: { mainHeader, subHeader, teamName, navButton },
+  components: { mainHeader, subHeader, teamName, navButton, blogFeed },
 }
 </script>
--- src/views/blog.vue
+++ src/views/blog.vue
@@ -3,59 +3,17 @@
     <mainHeader :text="'BRANDINGSチームブログ'" />
     <p>練習や試合のたびに選手・スタッフが持ち回りで記事を更新しています！（最近少しサボり気味...）</p>
     <p class="red--text">※記事をクリックすると別タブでブログを開きます。</p>
-    <div class="text-center my-8" v-show="loading">
-      <v-progress-circular indeterminate color="primary" />
-    </div>
-    <v-card
-      class="mb-6"
-      v-for="( item, i ) in items" :key="i"
-      :href="item.link"
-      target="_blank"
-    >
-      <v-img height="150px" :src="feedImagePath( item )" />
-      <v-card-title class="font-weight-bold">{{ feedTitle( item ) }}</v-card-title>
-      <v-card-text>{{ item.contentSnippet }}</v-card-text>
-    </v-card>
+    <blogFeed />
     <navButton label="アメブロで全部の記事を見る" href="https://ameblo.jp/brandings/" />
   </div>
 </template>
 
-<style lang="sass" scoped>
-</style>
-
 <script>
-const FEED_URL = "https://rss.app/feeds/snymP4nvS73euZij.xml";
-
-import Parser from "rss-parser";
-
 import mainHeader from "@/components/main-header";
 import navButton from "@/components/nav-button";
+import blogFeed from "@/components/blog-feed";
 
 export default {
-  data () {
-    return { items: [], loading: true }
-  },
-  components: { mainHeader, navButton },
-  created: function() {
-    let parser = new Parser();
-
-    parser.parseURL( FEED_URL, ( _, feed ) => {
-      this.items = feed.items;
-      this.loading = false;
-    });
-  },
-  methods: {
-    feedImagePath( item ) {
-      const regExp = /src="(.+\.jpg)"/g,
-            DUMMY_IMAGE_PATH = require( "@/assets/images/dummy_feed_image.jpeg" );
-
-      return regExp.exec( item.content )[ 1 ] || DUMMY_IMAGE_PATH;
-    },
-    feedTitle( item ) {
-      const regExp = /ブランディングス：(.*)$/g;
-
-      return regExp.exec( item.title )[ 1 ];
-    }
-  }
+  components: { mainHeader, navButton, blogFeed },
 }
 </script>
