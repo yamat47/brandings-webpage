1d4d3063c99643978183f589b47a801aecb0e8db
yamaguchi.takuya <yamaguchi.takuya.lr@gmail.com>
2019-08-16 11:22:10 +0900

    Adjust footer padding.

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <v-footer flex row>
-        <div class="col-6 col-md-4">
+        <div class="px-0 col-4 col-sm-6 col-md-4">
           <a v-for="link in links.sns" :key="link.id" :href="link.url" target="_blank" class="snsLink">
             <v-icon class="primary--text">{{ link.faIcon }}</v-icon>
             <span class="d-none d-sm-inline font-weight-bold">{{ link.name }}</span>
           </a>
         </div>
-        <span class="primary--text font-weight-bold col-6 col-md-4 text-right text-md-center">&copy; {{ new Date().getFullYear() }} BRANDINGS</span>
-        <span class="primary--text col-12 col-md-4 text-center text-md-right">Powered by <a href="https://twitter.com/yamat47" target="_blank">@yamat47</a></span>
+        <span class="primary--text px-0 font-weight-bold col-8 col-sm-6 col-md-4 text-right text-md-center">&copy; {{ new Date().getFullYear() }} BRANDINGS</span>
+        <span class="primary--text px-0 col-12 col-md-4 text-center text-md-right">Powered by <a href="https://twitter.com/yamat47" target="_blank">@yamat47</a></span>
     </v-footer>
   </div>
 </template>
@@ -17,6 +17,8 @@
 a.snsLink
   text-decoration: none
   margin-right: 1rem
+  &:last-child
+    margin-right: 0
   i
     margin-right: .5rem
 </style>
