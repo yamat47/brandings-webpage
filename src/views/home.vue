5094c366443256620860a1a8e4f61cf717b3a7f4
vagrant <vagrant@localhost.localdomain>
2019-10-19 14:44:55 +0000

    Show schedule via Jsonbox.

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,6 +6,14 @@
     <p>日本プライベートフットボール協会（通称JPFF）に所属し、ハーバーボウルやオーシャンボウルといったボウルゲームでの勝利を目指して日々練習や試合に取り組んでいます。</p>
     <p>「アメフトはしたいけど週何日も練習に参加できない...。」「学生の頃ほどのきつい練習ではなく楽しくプレーしたい！」といった方にはぴったりのチームです。ぜひホームページでチームの雰囲気を感じていただき少しでも興味を持ってもらえると嬉しいです！</p>
     <navButton label="入部案内" to="join_us" />
+    <subHeader icon="fas fa-football-ball" text="次回の練習・試合の予定" />
+    <div class="text-center my-8" v-show="scheduleLoading">
+      <v-progress-circular indeterminate color="primary" />
+    </div>
+    <v-list class="transparent pt-0" v-show="!scheduleLoading">
+      <scheduleFeedItem :event="this.latestEvent" :key="this.latestEvent.name" />
+    </v-list>
+    <navButton label="BRANDINGSスケジュール" to="schedule" />
     <subHeader icon="fas fa-football-ball" text="チームブログの最新記事" />
     <blogFeed :limit="3" />
     <navButton label="BRANDINGSチームブログ" to="blog" />
@@ -23,9 +31,23 @@ import mainHeader from "@/components/main-header";
 import subHeader from "@/components/sub-header";
 import teamName from "@/components/team-name";
 import navButton from "@/components/nav-button";
+import scheduleFeedItem from "@/components/schedule-feed-item";
 import blogFeed from "@/components/blog-feed";
 
+import axios from "axios";
+
 export default {
-  components: { mainHeader, subHeader, teamName, navButton, blogFeed },
+  components: { mainHeader, subHeader, teamName, navButton, scheduleFeedItem, blogFeed },
+  data () {
+    return { latestEvent: false, scheduleLoading: true }
+  },
+  mounted () {
+    axios
+      .get( "https://jsonbox.io/box_brandings_schedule_v1" )
+      .then( ( response ) => {
+        this.latestEvent = response.data.filter( event => new Date ( event.start_time ) > new Date () ).pop();
+        this.scheduleLoading = false;
+      } );
+  }
 }
 </script>
--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <mainHeader :text="'スケジュール'" />
-    <p>準備中です...</p>
+    <div class="text-center my-8" v-show="loading">
+      <v-progress-circular indeterminate color="primary" />
+    </div>
+    <v-list class="transparent pt-0 mb-6" v-show="!loading">
+      <template v-for="( item, i ) in items">
+        <scheduleFeedItem :event="item" :key="item.name" />
+        <v-divider :key="i" v-if="i + 1 < items.length" />
+      </template>
+    </v-list>
     <p>練習見学などご希望の方はお気軽に<navLink to="contact" label="こちら" />からお問い合わせください！</p>
   </div>
 </template>
@@ -15,8 +23,22 @@
 <script>
 import mainHeader from "@/components/main-header";
 import navLink from "@/components/nav-link";
+import scheduleFeedItem from "@/components/schedule-feed-item";
+
+import axios from "axios";
 
 export default {
-  components: { mainHeader, navLink },
+  components: { mainHeader, navLink, scheduleFeedItem },
+  data () {
+    return { items: [], loading: true }
+  },
+  mounted () {
+    axios
+      .get( "https://jsonbox.io/box_brandings_schedule_v1" )
+      .then( ( response ) => {
+        this.items = response.data.slice( 0, 5 );
+        this.loading = false;
+      } );
+  }
 }
 </script>
